64fc9fb769c5ce813f79aaf390e26860a5df7d72
aedelcarmen <adeagu_ap@hotmail.com>
2023-12-28 19:38:48 -0300

    principal

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,6 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
+import {createAccessToken} from "../libs/jwt.js";
 
 export const register = async (req, res) => {
   const { email, password, username } = req.body;
@@ -12,34 +12,19 @@ export const register = async (req, res) => {
       email,
       password: passwordHash,
     });
+
     const userSaved = await newUser.save();
-
-    jwt.sign(
-      {
-        id: userSaved._id,
-      },
-      "secret123",
-      {
-        expiresIn: "1d",
-      },
-      (err, token) => {
-        if (err) console.log(err);
-        res.cookie("token", token);
-        res.json({
-          message: "User created successfully",
-        });
-      }
-    );
-
-    // res.json({
-    //  id: userSaved._id,
-    //  username: userSaved.username,
-    //  email: userSaved.email,
-    //  createdAt: userSaved.createdAt,
-    //  updatedAt: userSaved.updatedAt,
-    //});
+    const token = await createAccessToken({ id: userSaved._id });
+    res.cookie("token", token);
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+    });
   } catch (error) {
-    console.log(error);
+     res.status(500).json({message:error.message});
   }
 };
 
