d4b6c2451236226448424bc7c40e3d25867ebd3d
aedelcarmen <adeagu_ap@hotmail.com>
2023-12-30 22:08:08 -0300

    principal

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -63,8 +63,15 @@ export const logout = (req, res) => {
   return res.sendStatus(200);
 };
 
-export const profile = (req, res) => {
-  const userFound = User.findById(req.user.id);
+export const profile = async (req, res) => {
+  const userFound = await User.findById(req.user.id);
   if (!userFound) return res.status(400).json({ message: "User not found" });
-  res.send("profile");
+
+  return res.json({
+    id: userFound._id,
+    username: userFound.username,
+    email: userFound.email,
+    createdAt: userFound.createdAt,
+    updatedAt: userFound.updatedAt,
+  });
 };
