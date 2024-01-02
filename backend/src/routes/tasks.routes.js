135de7ac842651fc6e377d79416e0a30b61f8754
aedelcarmen <adeagu_ap@hotmail.com>
2024-01-01 22:13:18 -0300

    principal

--- backend/src/routes/tasks.routes.js
+++ backend/src/routes/tasks.routes.js
@@ -1,8 +1,19 @@
-import {Router} from "express"
-import { authRequired } from "../middlewares/validateToken.js"
+import { Router } from "express";
+import { authRequired } from "../middlewares/validateToken.js";
+import {
+  getTasks,
+  getTask,
+  createTask,
+  updateTask,
+  deleteTask,
+} from "../controllers/tasks.controllers.js";
 
-const router = Router()
+const router = Router();
 
-router.get("/tasks", authRequired, (req,res) => res.send ("tasks"))
+router.get("/tasks", authRequired, getTasks);
+router.get("/tasks/:id", authRequired, getTask);
+router.post("/tasks", authRequired, createTask);
+router.delete("/tasks/:id", authRequired, deleteTask);
+router.put("/tasks/:id", authRequired, updateTask);
 
-export default router
+export default router;
