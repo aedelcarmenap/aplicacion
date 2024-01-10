2fd78e6d55c571a80f6c567ab99894ea5a766707
aedelcarmen <adeagu_ap@hotmail.com>
2024-01-10 20:55:02 -0300

    principal

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -7,7 +7,8 @@ export const register = async (req, res) => {
 
   try {
     const userFound = await User.findOne({ email });
-    return res.status(400).json(["the email is already in use"]);
+    if (userFound)
+      return res.status(400).json(["the email is already in use"]);
 
     const passwordHash = await bcrypt.hash(password, 10);
 
